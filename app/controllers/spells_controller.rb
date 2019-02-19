f3c3f74c0ffc15fd4a59666345be66a3ef869e21
delurfangs <delurfangs@gmail.com>
2019-02-19 13:40:16 -0600

    fixed eddit page to properly pull currrent values

--- app/controllers/spells_controller.rb
+++ app/controllers/spells_controller.rb
@@ -28,6 +28,16 @@ class SpellsController < ApplicationController
 		@spell
 	end
 
+	def update
+		@spell = Spell.find( params[:id])
+		if @spell.update_attributes(spell_params)
+			flash[:success] = "Spell Updated!"
+			redirect_to spell_path(id: params[:id])
+		else 
+			render action: :edit
+		end
+	end
+
 	private
 		def spell_params
 			params.require(:spell).permit(:name, :Level, :school, :Bard, :Cleric, :Druid, :Paladin, :Ranger, :Sorcerer, :Warlock, :Wizard, :'cast time', :Range, :distance, :Verbal, :Somatic, :Material, :'Other Components', :Duration, :Attack, :Source, :Description, :Concentration, :Ritual, :Area, :Page)
